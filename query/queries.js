f11e00da2d2c77aad0bc4ca53cc3dbbb8da94683
Riu <josemanuelriu@gmail.com>
2026-01-14 19:03:14 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -20,10 +20,10 @@ db.restaurants.find({"borough":"Bronx"},{ _id: 0 }).limit(5)
 db.restaurants.find({"borough":"Bronx"},{ _id: 0 }).skip(5).limit(5)
 
 // 8. Trobar els restaurants amb un score de més de 90.
-
+db.restaurants.find({"grades.score":{$gt:90}}, { _id: 0 })
 
 // 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
-
+db.restaurants.find({ "grades.score": { $gt: 80, $lt: 100 } }, { _id: 0 })
 
 // 10. Trobar els restaurants amb longitud menor que -95.754168.
 
