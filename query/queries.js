768d359d312282fc849ad897656df6e4dbeb7d5a
Riu <josemanuelriu@gmail.com>
2026-01-14 17:23:45 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -8,16 +8,16 @@ db.restaurants.find({},{ "restaurant_id": 1, "name": 1, "_id": 0 })
 db.restaurants.find({},{ "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1, "_id": 0 })
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
-
+db.restaurants.find({},{ "restaurant_id": 1, "name": 1, "borough": 1, "address.zipcode": 1, "_id": 0 })
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-
+db.restaurants.find({"borough":"Bronx"},{ _id: 0 })
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
-
+db.restaurants.find({"borough":"Bronx"},{ _id: 0 }).limit(5)
 
 // 7. Mostrar el pròxims 5 restaurants després de saltar els primers 5 del Bronx.
-
+db.restaurants.find({"borough":"Bronx"},{ _id: 0 }).skip(5).limit(5)
 
 // 8. Trobar els restaurants amb un score de més de 90.
 
