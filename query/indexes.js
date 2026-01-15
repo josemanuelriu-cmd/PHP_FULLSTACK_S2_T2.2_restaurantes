cdb87aff54915bde35284441dfadd5954b6d8480
Riu <josemanuelriu@gmail.com>
2026-01-15 12:04:36 +0100

    Delete index definitions in indexes.js
    Remove index creation for restaurants collection

--- query/indexes.js
+++ query/indexes.js
@@ -1,5 +1,1 @@
 //Use this file to define indexes for your queries
-db.restaurants.createIndex({ name: 1 });
-db.restaurants.createIndex({ grades.score: 1 });
-db.restaurants.createIndex({ borough: 1 });
-db.restaurants.createIndex({ cuisine: 1 });
